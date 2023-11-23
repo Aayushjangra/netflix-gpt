7900b5d573ac009c73f27c60229c34386b79ac94
Aayush Jangra <118102480+Aayushjangra@users.noreply.github.com>
2023-11-23 23:59:33 +0530

    Browser page

--- src/Utils/firebase.js
+++ src/Utils/firebase.js
@@ -23,6 +23,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
- const auth = getAuth();
- export getAuth 
+export const auth = getAuth();
+
 
